014fd51ecb202c43c758542a42cd59ec3572dc9c
Hamed <hamedahmedhamed100@gmail.com>
2023-06-08 18:59:46 +0200

    done with scrapy

--- scrapy/scrapy_pr/scrapy_pr/spiders/spider-scrapy.py
+++ scrapy/scrapy_pr/scrapy_pr/spiders/spider-scrapy.py
@@ -1,64 +1,99 @@
 import scrapy
+import pandas as pd
 import csv
-
-class Link(scrapy.Item):
-    link = scrapy.Field()
 
 class CountrySpider(scrapy.Spider):
     name = 'country_spider'
     start_urls = ['http://data.un.org/']
-    ScrapMin100 = True  # Set this to False to get all links
-    max_links = 10 if ScrapMin100 else None
+    ScrapMin100 = False  # Set this to False to get all links
+    max_links = 100 if ScrapMin100 else None
     processed_links = 0
     linksList = []
-    country_data = []
+    headers = []
+    data = []
+    df = pd.DataFrame()
+    
+    def __init__(self):
+        self.master_df = pd.DataFrame()  # master dataframe
 
     def parse(self, response):
         xpath = '//div[contains(@class, "CountryList")]//a[starts-with(@href, "en/iso")]/@href'
 
         selection = response.xpath(xpath).getall()
-        
+
         selection = response.xpath(xpath)
         for s in selection:
             if self.max_links and self.processed_links >= self.max_links:
                 break
             self.processed_links += 1
-            l = Link()
-            l['link'] = 'http://data.un.org/' + s.get()
-            link = l['link']
-
-            self.linksList.append(link)
-            yield scrapy.Request(link, callback=self.parse_country)
+            self.linksList.append('http://data.un.org/' + s.get())
+            yield scrapy.Request(self.linksList[-1], callback=self.parse_country)
 
     def parse_country(self, response):
         # Extract country name
         country_name = response.xpath('//td[@class="countrytable"]/text()').get()
 
-        # Extract table headers
-        headers = response.xpath('//details[summary="Economic indicators"]/table/thead/tr/td/text()').getall()
-        headers = ['Country Name', 'Year'] + headers[0:]  # Add 'Country Name' and 'Year' to headers list
+        # Extract table headers (years)
+        header_years = response.xpath('//summary[text()="Economic indicators"]/following-sibling::table//thead/tr/td/text()').getall()
+        header_years = [year.strip() for year in header_years]
+        self.headers = ['Country Name', 'Year'] + header_years
+        indicators = [
+        'GDP: Gross domestic product',
+        'GDP growth rate',
+        'GDP per capita',
+        'Economy: Agriculture',
+        'Economy: Industry',
+        'Economy: Services and other activity',
+        'Unemployment',
+        'Labour force participation rate',
+        'Balance of payments, current account',
+        'CPI: Consumer Price Index'
+    ]
+       
+         # Create a dataframe for the country
+        data = {
+            'CountryName': country_name,
+            'Year': header_years,
+        }
 
-        # Extract table data
-        data_rows = response.xpath('//details[summary="Economic indicators"]/table/tbody/tr')
-        for row in data_rows:
-            # Extract year from the first column of each row
-            year = row.xpath('td[1]/small/text()').get()
+        for i, indicator in enumerate(indicators):
+            data[indicator] = 'NA'
+            
+        # Create a dataframe for the country
+        self.df = pd.DataFrame(data)
 
-            # Extract values for the specified headers
-            values = row.xpath('td[position() > 1]/small/text()').getall()
+        # Initialize data lists for each header
+        self.data = [[] for _ in self.headers]
 
-            # Create a dictionary with the extracted data
-            country_data = dict(zip(headers, [country_name, year] + values))
+       
+        rowData = dict()
+        rows = response.xpath('//summary[text()="Economic indicators"]/following-sibling::table//tbody/tr')
+        for row in rows:
+            # Check if the row starts with any of the indicators
+            indicator = row.xpath('td[1]/text()').get()
+            if any(indicator.startswith(ind) for ind in indicators):
+                # Extract the values under each header for the row
+                values = row.xpath('td[position()>1]/small/text()').getall()
+                values = [value.strip() if value.strip() != '...' else 'NA' for value in values]
+                rowData[indicator] = values
+            
+        
+      
 
-            # Append the country data to the spider's country_data list
-            self.country_data.append(country_data)
+        for i in range(len(header_years)):
+            for indicator, values in rowData.items():
+                self.df.loc[i, indicator] = values[i]
+        
+       
+        # Append the individual country dataframe to the master dataframe
+        self.master_df = pd.concat([self.master_df, self.df], ignore_index=True)
 
-        # Check if all links have been processed
-        if self.processed_links == len(self.linksList):
-            # Write the country data to a CSV file
-            with open('country_data.csv', 'w', newline='') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=headers)
-                writer.writeheader()
-                writer.writerows(self.country_data)
 
-            self.log(f'Country data has been saved to country_data.csv')
+    def output_csv(self):
+    # Output master dataframe as a CSV file
+        self.master_df.to_csv('countries.csv', index=False)
+    def close(self, reason):
+        self.output_csv()  # This will write the final DataFrame to a csv
+
+        
+
